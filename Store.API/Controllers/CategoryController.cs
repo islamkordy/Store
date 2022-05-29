d7c6f882a74282b4cbc79a2d229d3f63010eb12e
islam kordy <Islam.Kordy@endava.com>
2022-05-29 22:04:33 +0200

    Add store.API project

---
+++ Store.API/Controllers/CategoryController.cs
@@ -0,0 +1,57 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Store.Application.Features.Categories.Commands.CreateCategory;
+using Store.Application.Features.Categories.Queries;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Store.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CategoryController : ControllerBase
+    {
+        private readonly IMediator mediator;
+
+        public CategoryController(IMediator mediator)
+        {
+            this.mediator = mediator;
+        }
+
+        [HttpGet("getCategoriesList")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        public async Task<ActionResult<List<CategoryListVM>>> GetCategoriesList()
+        {
+            var dtos = await mediator.Send(new GetCategoriesListQuery());
+
+            return Ok(dtos);
+        }
+
+        [HttpGet("getCategoryById")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        public async Task<ActionResult<CategoryVM>> GetCategoryById(Guid Id)
+        {
+            var dtos = await mediator.Send(new GetCategoryByIdQuery());
+
+            return Ok(dtos);
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<CategoryVM>> Get()
+        {
+            var dtos = await mediator.Send(new GetCategoryByIdQuery());
+
+            return Ok(dtos);
+        }
+
+        [HttpPost("addCategory")]
+        public async Task<ActionResult<CreateCategoryCommandResponse>> Create([FromBody] CreateCategoryCommand createCategoryCommand)
+        {
+            var response = await mediator.Send(createCategoryCommand);
+
+            return Ok(response);
+        }
+    }
+}
--- Store.API/Startup.cs
+++ Store.API/Startup.cs
@@ -1,3 +1,4 @@
+using MediatR;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -10,6 +11,7 @@ using Microsoft.OpenApi.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Threading.Tasks;
 
 namespace Store.API
@@ -26,6 +28,8 @@ namespace Store.API
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            //services.AddTransient<IMediator, Mediator>();
+            services.AddMediatR(typeof(Startup).GetTypeInfo().Assembly);
 
             services.AddControllers();
             services.AddSwaggerGen(c =>
