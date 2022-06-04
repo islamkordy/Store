d042c3dbe67f72dd1e91c7aa395e4abd08ab7e93
islam kordy <Islam.Kordy@endava.com>
2022-06-04 11:19:32 +0200

    Add Migration and update database

--- Store.API/Startup.cs
+++ Store.API/Startup.cs
@@ -1,29 +1,25 @@
 using MediatR;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
-using System;
-using System.Collections.Generic;
-using System.Linq;
+using Persistence;
 using System.Reflection;
-using System.Threading.Tasks;
 
 namespace Store.API
 {
     public class Startup
     {
+        public IConfiguration _configuration { get; }
+
         public Startup(IConfiguration configuration)
         {
-            Configuration = configuration;
+            _configuration = configuration;
         }
 
-        public IConfiguration Configuration { get; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
@@ -32,6 +28,8 @@ namespace Store.API
             services.AddMediatR(typeof(Startup).GetTypeInfo().Assembly);
 
             services.AddControllers();
+            services.AddDbContext<StoreDbContext>(options =>
+               options.UseSqlServer(_configuration.GetConnectionString("StoreDbConnection")));
             services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "Store.API", Version = "v1" });
