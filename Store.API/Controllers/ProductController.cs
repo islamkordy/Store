639e169b1e6dd259d317f857d0c1397e6421beab
islam kordy <Islam.Kordy@endava.com>
2022-07-01 23:19:23 +0200

    Add Logic for Product getList and getById

--- Store.API/Controllers/ProductController.cs
+++ Store.API/Controllers/ProductController.cs
@@ -2,6 +2,10 @@
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Store.Application.Features.Products.Commands;
+using Store.Application.Features.Products.Commands.DeleteProduct;
+using Store.Application.Features.Products.Queries.GetProductById;
+using Store.Application.Features.Products.Queries.GetProductsList;
+using System;
 using System.Threading.Tasks;
 
 namespace Store.API.Controllers
@@ -23,6 +27,35 @@ namespace Store.API.Controllers
         public async Task<ActionResult<CreateProductCommandResponse>> Create([FromBody] CreateProductCommand createProductCommand)
         {
             var response = await _mediator.Send(createProductCommand);
+
+            return Ok(response);
+        }
+
+        [HttpGet("getProductsList")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        public async Task<ActionResult<CreateProductCommandResponse>> GetProductsList()
+        {
+            var response = await _mediator.Send(new GetProductListQuery());
+
+            return Ok(response);
+        }
+
+        [HttpGet("getProductById")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        public async Task<ActionResult<CreateProductCommandResponse>> GetProductById(Guid Id)
+        {
+            var response = await _mediator.Send(new GetProductByIdQuery() { Id = Id});
+
+            return Ok(response);
+        }
+
+        [HttpDelete]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        public async Task<ActionResult> DeleteProduct([FromBody] DeleteProductCommand deleteProductCommand)
+        {
+            var response = await _mediator.Send(new DeleteProductCommand());
 
             return Ok(response);
         }
